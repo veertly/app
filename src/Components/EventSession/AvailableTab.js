27ae4cdf21fc8f255367370b8d09e9ed6811221b
joaoaguiam <joaoaguiam@gmail.com>
2020-04-15 20:53:26 +0200

    Grey icon when user is on the main stage

--- src/Components/EventSession/AvailableTab.js
+++ src/Components/EventSession/AvailableTab.js
@@ -237,7 +237,7 @@ export default function (props) {
                     vertical: "bottom",
                     horizontal: "right",
                   }}
-                  badgeContent={<ConferenceIcon style={{ heigth: "0.85em", width: "0.85em" }} />}
+                  badgeContent={<ConferenceIcon style={{ heigth: "0.85em", width: "0.85em", color: "#666" }} />}
                 >
                   {participantAvatar}
                 </Badge>
