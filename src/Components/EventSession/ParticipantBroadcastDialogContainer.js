7ed3a58f75e3d07698e7bfea6e613576c312c68e
Joao Aguiam <joaoaguiam@gmail.com>
2020-06-24 23:37:58 +0100

    fix issue with backstage and close on broadcast message

--- src/Components/EventSession/ParticipantBroadcastDialogContainer.js
+++ src/Components/EventSession/ParticipantBroadcastDialogContainer.js
@@ -2,35 +2,40 @@ import React, { useContext, useState, useEffect } from "react";
 import BroadcastMessagesContext from "../../Contexts/BroadcastMessagesContext";
 // import { useSelector } from "react-redux";
 // import { isEventOwner } from "../../Redux/eventSession";
-import { Dialog, DialogContent, DialogActions, DialogTitle, Button, makeStyles, Typography } from "@material-ui/core";
+import {
+  DialogContent,
+  DialogActions,
+  DialogTitle,
+  Button,
+  makeStyles,
+  Typography
+} from "@material-ui/core";
+import DialogClose from "../Misc/DialogClose";
 
 const useStyles = makeStyles((theme) => ({
   button: { margin: theme.spacing(2) },
   dialogTitle: { color: theme.palette.primary.main }
 }));
 
-
 const ParticipantBroadcastDialogContainer = React.memo(() => {
   const classes = useStyles();
-  const {
-    activeBroadcastMessage
-  } = useContext(BroadcastMessagesContext);
-  
+  const { activeBroadcastMessage } = useContext(BroadcastMessagesContext);
+
   // const isOwner = useSelector(isEventOwner);
 
   const [showDialog, setShowDialog] = useState(false);
 
   const handleClose = () => {
     setShowDialog(false);
-  }
+  };
 
   useEffect(() => {
     setShowDialog(!!activeBroadcastMessage);
-  }, [activeBroadcastMessage, setShowDialog])
+  }, [activeBroadcastMessage, setShowDialog]);
 
   if (activeBroadcastMessage) {
     return (
-      <Dialog fullWidth open={showDialog} onClose={handleClose}>
+      <DialogClose fullWidth open={showDialog} onClose={handleClose}>
         <DialogTitle className={classes.dialogTitle}>Announcement</DialogTitle>
         <DialogContent>
           <Typography variant="body1">
@@ -41,14 +46,15 @@ const ParticipantBroadcastDialogContainer = React.memo(() => {
           <Button
             onClick={handleClose}
             color="primary"
+            variant="contained"
             className={classes.button}
           >
-            Okay
+            Ok
           </Button>
         </DialogActions>
-      </Dialog>
-    )
-  };
+      </DialogClose>
+    );
+  }
   return null;
 });
 
