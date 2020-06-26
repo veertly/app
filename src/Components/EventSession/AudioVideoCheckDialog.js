28085fe1adde5925f9a9a0a4b663ee31cf3a3818
Harish Rana <r.shivi@yahoo.com>
2020-06-26 19:32:01 +0530

    fix: change selected input video from object to deviceId

--- src/Components/EventSession/AudioVideoCheckDialog.js
+++ src/Components/EventSession/AudioVideoCheckDialog.js
@@ -208,7 +208,7 @@ const AudioVideoCheckDialog = ({
   }
 
   const videoConstraint = {
-    deviceId: selectedVideoInput,
+    deviceId: selectedVideoInput ? selectedVideoInput.deviceId : undefined,
   };
 
   const handleNeedHelpClicked = () => {
