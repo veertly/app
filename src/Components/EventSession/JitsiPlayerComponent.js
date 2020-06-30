1e87bb9feef1882e459657621d73908ddb8d1509
Joao Aguiam <joaoaguiam@gmail.com>
2020-06-30 08:16:42 +0100

    Styling on the server down image

--- src/Components/EventSession/JitsiPlayerComponent.js
+++ src/Components/EventSession/JitsiPlayerComponent.js
@@ -5,7 +5,7 @@ import { JITSI_MEET_SDK_URL } from "../../Modules/jitsi";
 import useScript from "../../Hooks/useScript";
 import { ReactComponent as ServerDown } from "../../Assets/illustrations/undraw_server_down.svg";
 
-const useStyles = makeStyles((theme)=> ({
+const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
     height: "100%"
@@ -17,13 +17,14 @@ const useStyles = makeStyles((theme)=> ({
     alignItems: "center",
     width: "100%",
     height: "100%",
-    "& > * + *" : {
-      marginTop: theme.spacing(1),
-    },
+    backgroundColor: "white",
+    "& > * + *": {
+      marginTop: theme.spacing(1)
+    }
   }
 }));
 
-const JitsiPlayerComponent =({
+const JitsiPlayerComponent = ({
   avatarUrl,
   displayName,
   sessionId,
@@ -32,22 +33,19 @@ const JitsiPlayerComponent =({
   showJitsiLogo,
   subject,
   roomName,
-  onVideoConferenceJoined=()=> {},
-  onVideoConferenceLeft=()=>{},
-  callEndedCb=()=>{},
-  jitsiServer=JITSI_MEET_SDK_URL,
+  onVideoConferenceJoined = () => {},
+  onVideoConferenceLeft = () => {},
+  callEndedCb = () => {},
+  jitsiServer = JITSI_MEET_SDK_URL
 }) => {
-
-  const [loaded, error] = useScript(
-    jitsiServer
-  );
+  const [loaded, error] = useScript(jitsiServer);
 
   const classes = useStyles();
   useJitsi({
     avatarUrl,
     displayName,
     sessionId,
-    containerId:"#conference-container",
+    containerId: "#conference-container",
     domain,
     showJitsiLogo,
     subject,
@@ -55,25 +53,37 @@ const JitsiPlayerComponent =({
     onVideoConferenceJoined,
     onVideoConferenceLeft,
     callEndedCb,
-    jitsiServer,
+    jitsiServer
   });
   const theme = useTheme();
-  if (!loaded) return (
-    <div id="conference-container">Loading...</div>
-  );
+  if (!loaded) return <div id="conference-container">Loading...</div>;
   if (error) {
     return (
       <div className={classes.errorContainer}>
-        <ServerDown height="50%" width="50%" fill={theme.palette.primary.main} />
-        <Typography variant="h6" >
+        <ServerDown
+          height="40%"
+          width="40%"
+          fill={theme.palette.primary.main}
+        />
+        {/* <Typography variant="h6" >
           Server configuration is wrong. Please contact the organizer or Veertly team
+        </Typography> */}
+        <Typography align="center" gutterBottom style={{ paddingTop: 16 }}>
+          It seems the video-conferencing system is down or not configured
+          correctly...
         </Typography>
+        <Typography variant="caption" display="block" align="center">
+          Please contact the event organizer or Veertly team
+        </Typography>
+        {/* <img
+          alt="Not available"
+          src={NoVideoImage}
+          className={classes.noVideoImage}
+        /> */}
       </div>
-    )
+    );
   }
-  return (
-    <div id="conference-container" className={classes.root} />
-  )
+  return <div id="conference-container" className={classes.root} />;
 };
 
 export default JitsiPlayerComponent;
